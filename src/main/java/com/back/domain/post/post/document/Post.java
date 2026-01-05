b3baaf10149b9271b61cefba40fe21a398b6c634
jj <syeonie0526@gmail.com>
2026-01-05 17:04:00 +0900

    0018 - Auditing 설정

--- src/main/java/com/back/domain/post/post/document/Post.java
+++ src/main/java/com/back/domain/post/post/document/Post.java
@@ -1,7 +1,10 @@
 package com.back.domain.post.post.document;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -11,7 +14,7 @@ import java.time.OffsetDateTime;
 
 @Data
 @Document(indexName = "posts")
-public class Post {
+public class Post implements Persistable<String> {
     @Id
     private String id;
 
@@ -28,12 +31,14 @@ public class Post {
             type = FieldType.Date,
             format = DateFormat.date_time
     )
+    @CreatedDate
     private OffsetDateTime createdAt;
 
     @Field(
             type = FieldType.Date,
             format = DateFormat.date_time
     )
+    @LastModifiedDate
     private OffsetDateTime lastModifiedAt;
 
     public Post(String title, String content, String author){
@@ -42,6 +47,11 @@ public class Post {
         this.author = author;
         this.createdAt = OffsetDateTime.now();
         this.lastModifiedAt = OffsetDateTime.now();
+    }
+
+    @Override
+    public boolean isNew() {
+        return id == null || (createdAt == null && lastModifiedAt == null);
     }
 
 }
