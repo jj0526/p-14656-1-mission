e55c05fb69576f4b8822e4254d12a4abbdcb7faf
jj <syeonie0526@gmail.com>
2026-01-05 16:36:04 +0900

    00017

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -43,4 +43,9 @@ public class PostService {
         post.setLastModifiedAt(java.time.OffsetDateTime.now());
         return postRepository.save(post);
     }
+
+    public void delete(String id) {
+        Post post = findById(id);
+        postRepository.delete(post);
+    }
 }
