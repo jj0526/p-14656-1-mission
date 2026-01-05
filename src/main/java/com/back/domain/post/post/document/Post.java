b34961ffa3ad4ef8b795dea19eff142efa84fc35
jj <syeonie0526@gmail.com>
2026-01-05 17:16:36 +0900

    0019 - BaseDocument 추상 클래스 추출 (리팩토링)

--- src/main/java/com/back/domain/post/post/document/Post.java
+++ src/main/java/com/back/domain/post/post/document/Post.java
@@ -1,22 +1,18 @@
 package com.back.domain.post.post.document;
 
+import com.back.global.BaseDocument;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.time.OffsetDateTime;
-
 @Data
 @Document(indexName = "posts")
-public class Post implements Persistable<String> {
-    @Id
-    private String id;
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Post extends BaseDocument<String> {
 
     @Field(type = FieldType.Text)
     private String title;
@@ -27,31 +23,10 @@ public class Post implements Persistable<String> {
     @Field(type = FieldType.Text)
     private String author;
 
-    @Field(
-            type = FieldType.Date,
-            format = DateFormat.date_time
-    )
-    @CreatedDate
-    private OffsetDateTime createdAt;
-
-    @Field(
-            type = FieldType.Date,
-            format = DateFormat.date_time
-    )
-    @LastModifiedDate
-    private OffsetDateTime lastModifiedAt;
 
     public Post(String title, String content, String author){
         this.title = title;
         this.content = content;
         this.author = author;
-        this.createdAt = OffsetDateTime.now();
-        this.lastModifiedAt = OffsetDateTime.now();
     }
-
-    @Override
-    public boolean isNew() {
-        return id == null || (createdAt == null && lastModifiedAt == null);
-    }
-
 }
